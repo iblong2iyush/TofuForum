ccb2a237e7ac6da2b575f37f264d3725130dc50c
eviltofu <eviltofu@mac.com>
2013-12-07 02:28:38 +0800

    Misc cleanup
    Cleaned up various messy items

--- app/scripts/controllers/signupForm.js
+++ app/scripts/controllers/signupForm.js
@@ -6,41 +6,42 @@
     
     var module = angular.module('tofuForumApp');
     module.controller('SignupformCtrl',function($scope,Util,Userbackend,Msgbox) {
-            $scope.hasSubmitted = false;
-            $scope.user = {};
-            $scope.signup = function(form) {
-                $scope.hasSubmitted = true;
-                if (!form.$valid) {
-                    Msgbox.alert('Please fill out all fields.');
-                    return;
-                }
-                // check to see if passwords and emails match
-                var newForm = angular.copy(form);
-                Util.removePropertiesWith(newForm,'$');
-                // send the request
-                // redirect to login screen if successful
-                // display error message if not
-            };
-            $scope.isNotMatchingEmails = function() {
-                var user = $scope.user;
-                if (!('userEmail' in user)) {
-                    return true;
-                }
-                if (!('userEmailConfirm' in user)) {
-                    return true;
-                }
-                return (user.userEmail !== user.userEmailConfirm);
-            };
-            $scope.isNotMatchingPasswords = function() {
-                var user = $scope.user;
-                if (!('userPassword' in user)) {
-                    return true;
-                }
-                if (!('userPasswordConfirm' in user)) {
-                    return true;
-                }
-                return (user.userPassword !== user.userPasswordConfirm);
-            };
-        });
+        $scope.hasSubmitted = false;
+        $scope.user = {};
+        var successCallback = function(){
+            // have to figure out what this is
+        };
+        var failureCallback = function(){
+            // yes this too
+        };
+        $scope.signup = function(form) {
+            $scope.hasSubmitted = true;
+            if (!form.$valid) {
+                Msgbox.alert('Please fill out all fields.');
+                return;
+            }
+            Userbackend.signup($scope.user,successCallback,failureCallback);
+        };
+        $scope.isNotMatchingEmails = function() {
+            var user = $scope.user;
+            if (!('userEmail' in user)) {
+                return true;
+            }
+            if (!('userEmailConfirm' in user)) {
+                return true;
+            }
+            return (user.userEmail !== user.userEmailConfirm);
+        };
+        $scope.isNotMatchingPasswords = function() {
+            var user = $scope.user;
+            if (!('userPassword' in user)) {
+                return true;
+            }
+            if (!('userPasswordConfirm' in user)) {
+                return true;
+            }
+            return (user.userPassword !== user.userPasswordConfirm);
+        };
+    });
     
 })();
--- app/scripts/services/userbackend.js
+++ app/scripts/services/userbackend.js
@@ -5,9 +5,16 @@
     'use strict';
 
     var module = angular.module('tofuForumApp');
-    module.factory('Userbackend', function () {
+    module.factory('Userbackend', function() {
         // AngularJS will instantiate a singleton by calling "new" on this function
         var service = {};
+        service.signup = function(data,successCallback,failureCallback) {
+            // We should sign up and have two callback functions
+            successCallback=false;
+            failureCallback=false;
+            console.log(data);
+        };
+
         return service;
     });
 
