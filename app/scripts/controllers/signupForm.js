4ce96c7734e7112ff3a6c31fc27aed2b6719afe6
eviltofu <eviltofu@mac.com>
2013-12-06 17:42:50 +0800

    Added input validation for signup form
    Used the ListController SignupForm and the inputs $error field to
    handle most of the validation messages.

--- app/scripts/controllers/signupForm.js
+++ app/scripts/controllers/signupForm.js
@@ -5,22 +5,43 @@
     'use strict';
     
     var module = angular.module('tofuForumApp');
-    module.controller('SignupformCtrl', function ($scope,Util,Message,MessageboxEvent,MessageboxAlertType) {
-        $scope.signup = function(form) {
-            console.log('We got it');
-            if (!form.$valid) {
-                console.log('Form is not valid');
-                Message.send(MessageboxEvent,{type:MessageboxAlertType,message:'Please fill out all fields of the form'});
-                return;
-            }
-            // check to see if passwords and emails match
-            var newForm = angular.copy(form);
-            Util.removePropertiesWith(newForm,'$');
-            // send the request
-            // redirect to login screen if successful
-            // display error message if not
-        };
-    });
+    module.controller('SignupformCtrl',function($scope,Util,Message,MessageboxEvent,MessageboxAlertType) {
+            $scope.hasSubmitted = false;
+            $scope.user = {};
+            $scope.signup = function(form) {
+                $scope.hasSubmitted = true;
+                if (!form.$valid) {
+                    console.log(form);
+                    Message.send(MessageboxEvent,{type:MessageboxAlertType,message:'Please fill out all fields.'});
+                    return;
+                }
+                // check to see if passwords and emails match
+                var newForm = angular.copy(form);
+                Util.removePropertiesWith(newForm,'$');
+                // send the request
+                // redirect to login screen if successful
+                // display error message if not
+            };
+            $scope.isNotMatchingEmails = function() {
+                var user = $scope.user;
+                if (!('userEmail' in user)) {
+                    return true;
+                }
+                if (!('userEmailConfirm' in user)) {
+                    return true;
+                }
+                return (user.userEmail !== user.userEmailConfirm);
+            };
+            $scope.isNotMatchingPasswords = function(fieldName) {
+                var user = $scope.user;
+                if (!('userPassword' in user)) {
+                    return true;
+                }
+                if (!('userPasswordConfirm' in user)) {
+                    return true;
+                }
+                return (user.userPassword !== user.userPasswordConfirm);
+            };
+        });
     
 })();
-
