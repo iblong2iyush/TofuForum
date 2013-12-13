c00e9c9960728a240f4b8d63fcd2f93f097bc18e
eviltofu <eviltofu@mac.com>
2013-12-13 22:29:54 +0800

    Added tests to signup form

--- test/spec/controllers/signupForm.js
+++ test/spec/controllers/signupForm.js
@@ -20,9 +20,36 @@
             });
         }));
         
-        it('should attach a list of awesomeThings to the scope', function () {
-            // expect(true).toBeTrue();
+        it('Test isNotMatchingPasswords Passwords Match', function () {
+            scope.user = {
+                'userPassword': 'asasasasas',
+                'userPasswordConfirm': 'asasasasas'
+            };
+            expect(scope.isNotMatchingPasswords()).toBe(false);
         });
+        
+        it('Test isNotMatchingPasswords Passwords Do Not Match', function () {
+            scope.user = {
+                'userPassword': 'asasasasas1',
+                'userPasswordConfirm': '1asasasasas'
+            };
+            expect(scope.isNotMatchingPasswords()).toBe(true);
+        });
+        
+        it('Test isNotMatchingPasswords userPassword is missing', function () {
+            scope.user = {
+                'userPasswordConfirm': '1asasasasas'
+            };
+            expect(scope.isNotMatchingPasswords()).toBe(true);
+        });
+        
+        it('Test isNotMatchingPasswords userPasswordConfirm is missing', function () {
+            scope.user = {
+                'userPassword': 'asasasasas1'
+            };
+            expect(scope.isNotMatchingPasswords()).toBe(true);
+        });
+
     });
     
 })();
