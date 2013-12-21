97c40c4e04e346c95d787a5b84bde460fb188095
eviltofu <eviltofu@mac.com>
2013-12-22 00:09:14 +0800

    Tidied up password page controller

--- app/scripts/controllers/password.js
+++ app/scripts/controllers/password.js
@@ -5,8 +5,7 @@
     'use strict';
 
     var module = angular.module('tofuForumApp');
-    module.controller('PasswordCtrl', function ($scope) {
-        $scope.form = {};
+    module.controller('PasswordCtrl', function () {
     });
 
 })();
