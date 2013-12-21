8762708a4ea0a851738e92c917d3716360bd3137
eviltofu <eviltofu@mac.com>
2013-12-22 00:07:31 +0800

    Used module var instead of doing a long chain

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,60 +4,41 @@
     
     'use strict';
     
-    angular.module('tofuForumApp', [
+    var module = angular.module('tofuForumApp', [
         'ui.bootstrap',
         'ngCookies',
         'ngResource',
         'ngSanitize',
         'ngRoute'
-    ])
-        .config(function (
-            $routeProvider,
-            $locationProvider) {
-            $routeProvider
-                .when('/login', {
-                    templateUrl: 'views/login.html',
-                    controller: 'LoginCtrl'
-                })
-                .when('/signup', {
-                    templateUrl: 'views/signup.html',
-                    controller: 'SignupCtrl'
-                })
-                .when('/main', {
-                    templateUrl: 'views/main.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/password', {
-                    templateUrl: 'views/password.html',
-                    controller: 'PasswordCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/main'
-                });
-            $locationProvider.html5Mode(false).hashPrefix('');
-        })
-        .run(function(
-            $rootScope,
-            $location,
-            Authentication){
-            var unauthenticatedRoutes = ['/login','/signup'];
-            var routeNeedsAuthentication = function(route) {
-                for (var index=0; index<unauthenticatedRoutes.length; index++ ) {
-                    if (unauthenticatedRoutes[index] === route) {
-                        return false;
-                    }
-                }
-                return true;
-            };
-            $rootScope.$on('$routeChangeStart', function () {
-                if (!routeNeedsAuthentication($location.url())){
-                    return;
-                }
-                if (Authentication.authenticated) {
-                    return;
-                }
-                $location.path('/login');
+    ]);
+    module.config(function ($routeProvider,$locationProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/signup', {
+                templateUrl: 'views/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .when('/main', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/password', {
+                templateUrl: 'views/password.html',
+                controller: 'PasswordCtrl'
+            })
+            .otherwise({
+                redirectTo: '/main'
             });
+        $locationProvider.html5Mode(false).hashPrefix('');
+    })
+        .run(function(
+            Navigation){
+            Navigation.addSafeRoute('/login');
+            Navigation.addSafeRoute('/signup');
+            Navigation.setDefaultRoute('/login');
         });
-
+    
 })();
