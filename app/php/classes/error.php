55bdbf0ebe1a12a51c01ec49e3f5b2ad3d3c116f
eviltofu <eviltofu@mac.com>
2013-12-27 03:42:00 +0800

    Refactored errors into their own class

---
+++ app/php/classes/error.php
@@ -0,0 +1,18 @@
+<?php
+
+class Error {
+
+    const resultOk = 0;
+    const sqlError = 1;
+    const duplicateNameError = 2;
+    const unknownError = 3;
+    const nonMatchingEmailError = 4;
+    const nonMatchingPasswordError = 5;
+    const missingFieldError = 6;
+    const namePasswordError = 7;
+    const accountError = 8;
+    const unauthenticatedError = 9;
+
+}
+
+?>
